a20b0cc488f6246435c37da64db80fa82ccb5ace
Maximilian Thompson <mthompson2@wpi.edu>
2019-02-03 17:08:58 -0500

    Print the data now.

--- Project2/Part2/ancestry.c
+++ Project2/Part2/ancestry.c
@@ -16,9 +16,34 @@ typedef struct {
 } ancestry_t;
 
 void init_ancestry(ancestry_t* a) {
-    for (int i = 0; i < 10; i++) a->ancestors[i] = 0;
-    for (int i = 0; i < 100; i++) a->siblings[i] = 0;
-    for (int i = 0; i < 100; i++) a->children[i] = 0;
+    for (int i = 0; i < 10; i++) a->ancestors[i] = -1;
+    for (int i = 0; i < 100; i++) a->siblings[i] = -1;
+    for (int i = 0; i < 100; i++) a->children[i] = -1;
+}
+
+void print_siblings(ancestry_t* a) {
+    printf("SIBLINGS:\n");
+    for (int i = 0; i < 100 && a->siblings[i] != -1; i++)
+        printf("%d, ", a->siblings[i]);
+    
+    printf("\n");
+}
+
+void print_children(ancestry_t* a) {
+    printf("CHILDREN:\n");
+    for (int i = 0; i < 100 && a->children[i] != -1; i++)
+        printf("%d, ", a->children[i]);
+    
+    printf("\n");
+}
+
+void print_ancestors(ancestry_t* a) {
+    printf("ANCESTORS:\n");
+    for (int i = 9; i >= 0; i--)
+        if (a->ancestors[i] != -1)
+            printf("%d -> ", a->ancestors[i]);
+    
+    printf("{target}\n\n");
 }
 
 int main () {
@@ -32,5 +57,9 @@ int main () {
         abort();
     }
 
-    printf("stuff goes here\n");
+    print_children(&ancestry);
+    print_siblings(&ancestry);
+    print_ancestors(&ancestry);
+
+    return 0;
 }
